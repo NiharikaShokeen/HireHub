9f75c3be7a7f6e718c2d7230141557f0b2ebac9b
Niharika Shokeen <niharika.shokeen05@gmail.com>
2026-04-18 22:36:32 +0530

    Connected Node.js with MySQL and added user registration

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+// MySQL Connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "hirehub"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("Database Error");
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
+
+// Home Page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Register Route
+app.post("/register", (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const sql = "INSERT INTO users (name,email,password,role) VALUES (?, ?, ?, ?)";
+
+  db.query(sql, [name, email, password, role], (err, result) => {
+    if (err) {
+      res.send("Registration Failed");
+    } else {
+      res.send("Registered Successfully ✅");
+    }
+  });
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
